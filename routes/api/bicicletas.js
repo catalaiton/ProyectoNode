8a6489d8d6d9fe1866045225a1c46d0cec592bf8
Yohana Avila <yohana.avila3009@gmail.com>
2023-05-15 22:20:12 -0500

    Método Delete funcionando

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -4,5 +4,6 @@ var bicicletaControler= require('../../controlers/api/bicicletaControllerAPI');
 
 router.get('/', bicicletaControler.bicicleta_list);
 router.post('/create', bicicletaControler.bicicleta_create);
+router.delete('/delete', bicicletaControler.bicicleta_delete);
 
 module.exports  = router;
