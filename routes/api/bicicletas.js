99941f2e607ff1b8c385f6166519e26f49ef9166
Yohana Avila <yohana.avila3009@gmail.com>
2023-05-15 22:12:18 -0500

    Método Post funcionando

--- routes/api/bicicletas.js
+++ routes/api/bicicletas.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var bicicletaControler= require('../../controlers/api/bicicletaControllerAPI'); 
 
 router.get('/', bicicletaControler.bicicleta_list);
+router.post('/create', bicicletaControler.bicicleta_create);
 
-module.exports = router;
+module.exports  = router;
