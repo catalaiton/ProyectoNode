706a2be3bfa6cf8fc6b8d2c905df95af561864a6
Yohana Avila <yohana.avila3009@gmail.com>
2023-05-26 00:16:18 -0500

    Agregando tercer test con 'jasmine' del método 'findById' que busca bicicletas

--- spec/models/bicicleta_test.spec.js
+++ spec/models/bicicleta_test.spec.js
@@ -1,5 +1,7 @@
 var Bicicleta= require('../../models/bicicleta');
 
+beforeEach(()=>{ Bicicleta.allBicis = [];});
+
 describe('Bicicleta.allBicis', ()=> {
     it('Comienza vacío', ()=> {
         expect(Bicicleta.allBicis.length).toBe(0);
@@ -12,5 +14,20 @@ describe('Bicicleta.add', ()=> {
         var a = new Bicicleta(1, 'rojo','urbana',[4.6281149,-74.0680332]);
         Bicicleta.add(a);
         expect(Bicicleta.allBicis[0]).toBe(a);
+    });
+});
+
+describe('Bicicleta.findById', ()=> {
+    it('Debe devolver la bici con id 1', ()=> {
+        expect(Bicicleta.allBicis.length).toBe(0);
+        var aBici = new Bicicleta(1, 'Verde','Urbana');
+        var aBici2 = new Bicicleta(2, 'Dorada','Montaña');
+        Bicicleta.add(aBici);   
+        Bicicleta.add(aBici2);
+        
+        var targetBici = Bicicleta.findById(1);
+        expect(targetBici.id).toBe(1);
+        expect(targetBici.color).toBe(aBici.color);
+        expect(targetBici.modelo).toBe(aBici.modelo);
     });
 });
