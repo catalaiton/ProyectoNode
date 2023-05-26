8e6abb771e9309b8bc2acb3a4b4d141d78920a17
Yohana Avila <yohana.avila3009@gmail.com>
2023-05-26 00:02:55 -0500

    Agregando segundo test con 'jasmine' del método que añade las bicicletas

--- spec/models/bicicleta_test.spec.js
+++ spec/models/bicicleta_test.spec.js
@@ -5,3 +5,12 @@ describe('Bicicleta.allBicis', ()=> {
         expect(Bicicleta.allBicis.length).toBe(0);
     });
 });
+
+describe('Bicicleta.add', ()=> {
+    it('Agregamos una', ()=> {
+        expect(Bicicleta.allBicis.length).toBe(0);
+        var a = new Bicicleta(1, 'rojo','urbana',[4.6281149,-74.0680332]);
+        Bicicleta.add(a);
+        expect(Bicicleta.allBicis[0]).toBe(a);
+    });
+});
