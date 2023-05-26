9763b45085997bcbb67eb646e51f8db68b343bcc
Yohana Avila <yohana.avila3009@gmail.com>
2023-05-25 21:13:59 -0500

    Corrección de numeración de marcadores en el mapa

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,18 +4,14 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([4.6281149,-74.0680332]).addTo(map);
-L.marker([4.5794549,-74.160091]).addTo(map);
-L.marker([4.5794537,-74.1988069]).addTo(map);
-
-// $.ajax({
-//     dataType: "json",
-//     url: "api/bicicletas",
-    
-//     success: function (result) {
-//         console.log(result);
-//         result.bicicletas.forEach(function(bici) {
-//             L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
-//         });
-//     }
-// })
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+ 
+    success: function (result) {
+        console.log(result);
+        result.bicicletas.forEach(function(bici) {
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
